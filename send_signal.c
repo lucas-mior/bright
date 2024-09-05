613904ead5124727da213d5f986b5556e3b5cf17
lucas-mior <lucas.mior@tutamail.com>
2024-09-05 10:35:24 -0300

    test: inline check_pid on send_signal.c

--- send_signal.c
+++ send_signal.c
@@ -29,13 +29,10 @@
 
 #include "bright.h"
 
-static pid_t check_pid(const char *, const char*);
-
 void
 send_signal(char *executable, int signal_number) {
     DIR *processes;
     struct dirent *process;
-    pid_t pid;
 
     if (!(processes = opendir("/proc"))) {
         error("Error opening /proc: %s\n", strerror(errno));
@@ -43,44 +40,33 @@ send_signal(char *executable, int signal_number) {
     }
 
     while ((process = readdir(processes))) {
-        if ((pid = check_pid(executable, process->d_name))) {
-            kill(pid, SIGRTMIN+signal_number);
-            break;
+        static char buffer[256];
+        static char command[256];
+        int pid;
+        FILE *cmdline;
+
+        if ((pid = atoi(process->d_name)) <= 0)
+            continue;
+
+        snprintf(buffer, sizeof (buffer), "/proc/%d/cmdline", pid);
+        buffer[sizeof (buffer) - 1] = '\0';
+
+        if (!(cmdline = fopen(buffer, "r")))
+            continue;
+        if (!fgets(command, sizeof(command), cmdline)) {
+            fclose(cmdline);
+            continue;
         }
+        command[strcspn(buffer, "\n")] = '\0';
+
+        if (!strcmp(command, executable))
+            kill(pid, SIGRTMIN+signal_number);
+
+        fclose(cmdline);
     }
 
     closedir(processes);
     return;
-}
-
-pid_t
-check_pid(const char *executable, const char *number) {
-    static char buffer[256];
-    static char command[256];
-    int pid;
-    FILE *cmdline;
-
-    if ((pid = atoi(number)) <= 0)
-        return 0;
-
-    snprintf(buffer, sizeof (buffer), "/proc/%d/cmdline", pid);
-    buffer[sizeof (buffer) - 1] = '\0';
-
-    if (!(cmdline = fopen(buffer, "r")))
-        return 0;
-    if (!fgets(command, sizeof(command), cmdline)) {
-        fclose(cmdline);
-        return 0;
-    }
-    command[strcspn(buffer, "\n")] = '\0';
-
-    if (!strcmp(command, executable)) {
-        fclose(cmdline);
-        return pid;
-    }
-
-    fclose(cmdline);
-    return 0;
 }
 
 #endif
