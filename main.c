b3f001472ca93f79137aeb4957771377a038a728
lucas-mior <lucas.mior@tutamail.com>
2025-10-17 15:08:18 -0300

    util.c: multiple fixes

--- main.c
+++ main.c
@@ -47,8 +47,7 @@ main(int argc, char *argv[]) {
     }
 
     for (ic = 0; ic < LENGTH(commands); ic += 1) {
-        if (!strcmp(argv[1], commands[ic].shortname)
-            || !strcmp(argv[1], commands[ic].longname)) {
+        if (!strcmp(argv[1], commands[ic].shortname) || !strcmp(argv[1], commands[ic].longname)) {
             spell_error = false;
             switch (ic) {
             case COMMAND_MORE:
@@ -62,14 +61,16 @@ main(int argc, char *argv[]) {
             }
         }
     }
-    if (spell_error)
+    if (spell_error) {
         main_usage(stderr);
+    }
 
-    out:
-    if (argc >= 3)
+out:
+    if (argc >= 3) {
         program_to_signal = argv[2];
-    else
+    } else {
         program_to_signal = NULL;
+    }
 
     SNPRINTF(max_bright.file, "%s/max_brightness", bright_directory);
     SNPRINTF(old_bright.file, "%s/brightness", bright_directory);
@@ -80,14 +81,15 @@ main(int argc, char *argv[]) {
         int last = max_bright.absolute;
         int first = last / 60;
         int n = NLEVELS - 2;
-        double m = (double) 1 / (double) (n - 1);
-        double quotient = pow((double) last / (double) first, m);
+        double m = (double)1 / (double)(n - 1);
+        double quotient = pow((double)last / (double)first, m);
 
         levels[0] = 0;
         levels[1] = 1;
         levels[2] = first;
-        for (int i = 3; i < NLEVELS - 1; i += 1)
-            levels[i] = (int) ((double) levels[i - 1] * quotient);
+        for (int i = 3; i < NLEVELS - 1; i += 1) {
+            levels[i] = (int)((double)levels[i - 1]*quotient);
+        }
         levels[NLEVELS - 1] = last;
     }
 
@@ -95,8 +97,7 @@ main(int argc, char *argv[]) {
 
     for (int i = 0; i < NLEVELS; i += 1) {
         old_bright.index = i;
-        if ((levels[i] <= old_bright.absolute)
-             && (old_bright.absolute < levels[i + 1])) {
+        if ((levels[i] <= old_bright.absolute) && (old_bright.absolute < levels[i + 1])) {
             break;
         }
     }
@@ -109,18 +110,20 @@ main(int argc, char *argv[]) {
         printf("🔆 %i", old_bright.index);
         exit(EXIT_SUCCESS);
     case COMMAND_LESS:
-        if (0 < old_bright.index)
+        if (0 < old_bright.index) {
             new_bright.index -= 1;
+        }
         break;
     case COMMAND_MORE:
-        if (old_bright.index < NLEVELS - 1)
+        if (old_bright.index < NLEVELS - 1) {
             new_bright.index += 1;
+        }
         break;
     case COMMAND_FULL:
         new_bright.index = NLEVELS - 1;
         break;
     default:
-        fprintf(stderr, "Unexpected value: %d\n", ic); 
+        fprintf(stderr, "Unexpected value: %d\n", ic);
         exit(EXIT_FAILURE);
     }
 
@@ -128,8 +131,7 @@ main(int argc, char *argv[]) {
         FILE *save;
 
         if (!(save = fopen(new_bright.file, "w"))) {
-            error("Can't open file for setting current brightness: %s\n",
-                  strerror(errno));
+            error("Can't open file for setting current brightness: %s\n", strerror(errno));
             exit(EXIT_FAILURE);
         }
         if (fprintf(save, "%d\n", levels[new_bright.index]) < 0) {
@@ -149,13 +151,11 @@ main(int argc, char *argv[]) {
             exit(EXIT_FAILURE);
         }
         if ((DWMBLOCKS2_BRIGHT.number = atol(DWMBLOCKS2_BRIGHT.string)) < 10) {
-            error("Invalid BRIGHT environment variable: %s.\n",
-                   DWMBLOCKS2_BRIGHT.string);
+            error("Invalid BRIGHT environment variable: %s.\n", DWMBLOCKS2_BRIGHT.string);
             exit(EXIT_FAILURE);
         }
 
-        send_signal(program_to_signal,
-                    (int)(SIGRTMIN + DWMBLOCKS2_BRIGHT.number));
+        send_signal(program_to_signal, (int)(SIGRTMIN + DWMBLOCKS2_BRIGHT.number));
     }
 
     exit(EXIT_SUCCESS);
@@ -174,8 +174,9 @@ get_bright(Brightness *bright) {
 
     if ((r = read(file, buffer, sizeof(buffer))) <= 0) {
         error("Can't read from file.");
-        if (r < 0)
+        if (r < 0) {
             error(": %s", strerror(errno));
+        }
         error(".\n");
         close(file);
         return;
@@ -192,8 +193,7 @@ main_usage(FILE *stream) {
     fprintf(stream, "usage: %s COMMAND [program_to_signal]\n", "bright");
     fprintf(stream, "Available commands:\n");
     for (uint i = 0; i < LENGTH(commands); i += 1) {
-        fprintf(stream, "%s | %-*s : %s\n",
-                commands[i].shortname, 8, commands[i].longname, 
+        fprintf(stream, "%s | %-*s : %s\n", commands[i].shortname, 8, commands[i].longname,
                 commands[i].description);
     }
     exit(stream != stdout);
