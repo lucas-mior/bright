18671c605e98f15bd59f415d452c77a3f6670666
lucas-mior <lucas.mior@tutamail.com>
2026-08-12 00:19:55 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ dir=$(dirname "$(readlink -f "$0")")
 . "$dir/cbase/common.sh"
 
 cd "$dir" || exit
-program=$(get_program "$0")
+program=$(common_get_program "$0")
 script=$(basename "$0")
-build_parse_args "$@"
+common_build_parse_args "$@"
 
-build_print_invocation "$script"
+common_build_print_invocation "$script"
 
 PREFIX="${PREFIX:-/usr/local}"
 DESTDIR="${DESTDIR:-/}"
@@ -19,7 +19,7 @@ DESTDIR="${DESTDIR:-/}"
 exe="bin/$program"
 mkdir -p "$(dirname "$exe")"
 
-CC=$(get_compiler "$mode")
+CC=$(common_get_compiler "$mode")
 
 CPPFLAGS="$CPPFLAGS -I$dir/cbase"
 
@@ -72,7 +72,7 @@ esac
 
 case "$mode" in
 fast_feedback)
-    build_tags
+    common_build_tags
     trace_on
     $CC $CPPFLAGS $CFLAGS -o "$exe" main.c $LDFLAGS && LC_ALL=C "$exe"
     trace_off
@@ -80,7 +80,7 @@ fast_feedback)
 uninstall)
     trace_on
     rm -f "${DESTDIR}${PREFIX}/bin/${program}"
-    uninstall_opt "${program}.1" "${DESTDIR}${PREFIX}/man/man1/${program}.1"
+    common_uninstall_opt "${program}.1" "${DESTDIR}${PREFIX}/man/man1/${program}.1"
     trace_off
     ;;
 check)
@@ -99,7 +99,7 @@ check)
 test)
     TEST_EXTRA_DEFS=-DCBASE_IMPLEMENT \
     TEST_MAXDEPTH=1 \
-        test "$target" .
+        common_test "$target" .
     ;;
 install)
     if [ ! -f "$exe" ]; then
@@ -107,11 +107,11 @@ install)
     fi
     trace_on
     install -Dm755 "$exe" "${DESTDIR}${PREFIX}/bin/${program}"
-    install_opt -Dm644 "${program}.1" "${DESTDIR}${PREFIX}/man/man1/${program}.1"
+    common_install_opt -Dm644 "${program}.1" "${DESTDIR}${PREFIX}/man/man1/${program}.1"
     trace_off
     ;;
 *)
-    build_tags
+    common_build_tags
 
     trace_on
     $CC $CPPFLAGS $CFLAGS -o "$exe" main.c $LDFLAGS
