3772608f050eddd5396349c68bbde04c6ab7f844
lucas-mior <lucas.mior@tutamail.com>
2026-08-13 17:01:02 -0300

    wip

--- build.sh
+++ build.sh
@@ -72,7 +72,7 @@ build)
     CFLAGS="$CFLAGS -O2 -flto -march=native -ftree-vectorize"
     ;;
 cross)
-    common_build_cross_all
+    common_build_cross_all windows
     cross="$target"
 
     CFLAGS="$CFLAGS -Wno-padded"
